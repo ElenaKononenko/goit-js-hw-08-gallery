e520fddfe23a1a1da9759a6a913fb5e4d2e3506f
ElenaKononenko <morra4444@gmail.com>
2020-11-21 14:35:27 +0200

    +ales

--- index.js
+++ index.js
@@ -4,30 +4,33 @@ const listGalleryRef = document.querySelector(".js-gallery");
 const lightboxRef = document.querySelector(".js-lightbox");
 const overlayRef = document.querySelector(".lightbox__overlay");
 const lightboxImageRef = document.querySelector(".lightbox__image");
+const galleryLinkRef = document.querySelector(".gallery__link");
 const btnCloseRef = document.querySelector(
   "button[data-action='close-lightbox']"
 );
 
 const createItemGallery = gallery.map(processGallery);
-listGalleryRef.append(...createItemGallery);
 
+listGalleryRef.append(...createItemGallery);
 listGalleryRef.addEventListener("click", onOpenModal);
 btnCloseRef.addEventListener("click", onCloseModal);
 overlayRef.addEventListener("click", onOverlayClose);
 
-function processGallery(item) {
+function processGallery(item, index) {
   const listItemGallery = document.createElement("li");
   listItemGallery.classList.add("gallery__item");
 
   const linkGallery = document.createElement("a");
   linkGallery.classList.add("gallery__link");
   linkGallery.setAttribute("href", item.original);
+  // linkGallery.setAttribute("data-index", index);
 
   const pictureGallery = document.createElement("img");
   pictureGallery.classList.add("gallery__image");
   pictureGallery.setAttribute("src", item.preview);
   pictureGallery.setAttribute("data-source", item.original);
   pictureGallery.setAttribute("alt", item.description);
+  pictureGallery.setAttribute("data-index", index);
 
   linkGallery.append(pictureGallery);
   listItemGallery.append(linkGallery);
@@ -36,15 +39,16 @@ function processGallery(item) {
 
 function onOpenModal(e) {
   e.preventDefault();
-  window.addEventListener("keydown", onPressEsc);
+  window.addEventListener("keydown", onPressKey);
   if (e.target.nodeName === "IMG") {
     lightboxRef.classList.add("is-open");
     lightboxImageRef.src = e.target.dataset.source;
+    lightboxImageRef.setAttribute("data-current", e.target.dataset.index);
   }
 }
 
 function onCloseModal(e) {
-  window.removeEventListener("keydown", onPressEsc);
+  window.removeEventListener("keydown", onPressKey);
   lightboxRef.classList.remove("is-open");
   lightboxImageRef.src = "";
 }
@@ -54,8 +58,29 @@ function onOverlayClose(e) {
     onCloseModal(e);
   }
 }
-function onPressEsc(e) {
+function onPressKey(e) {
   if (e.code === "Escape") {
     onCloseModal();
+  } else if (e.code === "ArrowLeft") {
+    modalLeft();
+  } else if (e.code === "ArrowRight") {
+    modalRight();
   }
+}
+
+function modalLeft() {
+  let n = Number(lightboxImageRef.dataset.current) - 1;
+  if (n < 0) {
+    n = gallery.length - 1;
+  }
+  lightboxImageRef.dataset.current = n;
+  lightboxImageRef.src = gallery[n].original;
+}
+function modalRight() {
+  let n = Number(lightboxImageRef.dataset.current) + 1;
+  if (n >= gallery.length) {
+    n = 0;
+  }
+  lightboxImageRef.dataset.current = n;
+  lightboxImageRef.src = gallery[n].original;
 }
